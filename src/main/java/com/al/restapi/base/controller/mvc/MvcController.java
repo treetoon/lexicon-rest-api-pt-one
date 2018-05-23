028b32476a43156f062247df913b2ced5f19aaab
Unknown <andreasl_93@hotmail.com>
2018-05-23 17:02:30 +0200

    mvc boilerplate added

--- src/main/java/com/al/restapi/base/controller/mvc/MvcController.java
+++ src/main/java/com/al/restapi/base/controller/mvc/MvcController.java
@@ -2,11 +2,17 @@ package com.al.restapi.base.controller.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MvcController {
-    @RequestMapping({"/", "index"})
-    public String getIndex(){
-        return "index";
+    @RequestMapping({"/", "default"})
+    public String getIndex(ModelAndView mv){
+        return "default";
+    }
+
+    @RequestMapping("test")
+    public String getTest(ModelAndView mv){
+        return "test";
     }
 }
