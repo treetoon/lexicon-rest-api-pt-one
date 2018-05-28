b2378068f7e6aec7b10577646ebe93e92ef1b6b0
Unknown <andreasl_93@hotmail.com>
2018-05-28 15:07:09 +0200

    added film pages

--- src/main/java/com/al/restapi/base/controller/mvc/MvcController.java
+++ src/main/java/com/al/restapi/base/controller/mvc/MvcController.java
@@ -1,11 +1,14 @@
 package com.al.restapi.base.controller.mvc;
 
+import com.al.restapi.base.model.FilmEntity;
 import com.al.restapi.base.service.film.FilmNotFoundException;
 import com.al.restapi.base.service.film.FilmService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MvcController {
@@ -16,16 +19,41 @@ public class MvcController {
         this.filmService = filmService;
     }
 
-    @RequestMapping({"/", "/index"})
+    @GetMapping({"/", "/index"})
     public String getIndex(Model m){
         return "index";
     }
 
-    @RequestMapping("/film")
-    public String getTest(Model m) {
+    @GetMapping("/film")
+    public String getFilm(Model m) {
+        return "pages/film/film";
+    }
+
+    @GetMapping("/film/add")
+    public String getFilmAdd(Model m) {
         try {
             m.addAttribute("filmList", filmService.findFilms());
         } catch (FilmNotFoundException e) {} //dw
-        return "film";
+        return "pages/film/film-add";
+    }
+
+    @PostMapping("/film/add")
+    public String postFilmAdd(Model m, FilmEntity film) {
+        System.out.println(film.getTitle());
+        return "pages/film/film-add";
+    }
+
+    @GetMapping("/film/list")
+    public String getFilmList(Model m) {
+        try {
+            m.addAttribute("filmList", filmService.findFilms());
+        } catch (FilmNotFoundException e) {} //dw
+        return "pages/film/film-list";
+    }
+
+    @DeleteMapping("/film/list")
+    public String deleteFilm(Model m) {
+
+        return "pages/film/film-list";
     }
 }
